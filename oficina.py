4ccca53397c29b5927aa4b4d94313b8b5ed0c67f
Victor Hugo Siqueira Alencar <d2025005350@unifei.edu.br>
2025-12-03 20:45:53 -0300

    classe Oficina

--- oficina.py
+++ oficina.py
@@ -1,30 +1,72 @@
-class Oficina:
-    def __init__(self,id, Nome, Valor ):
+class Oficina:  
+
+    def __init__(self):
+         
+        self.id=id
+        self.Nome = None
+        self.Valor = None
+        self.Historic= []
+
+    
+    def Registrar(self,id,Nome,Valor):
 
         if not isinstance(Nome, str) or Nome.strip() == "":
             raise ValueError("Nome inválido: deve ser texto não vazio.")
+        else:
+            self.Nome=Nome
 
-        if not isinstance(valor, float):
+        if not isinstance(Valor, (int,float)):
             raise TypeError("o valor deve ser um numero.")
-
-        if valor < 0 :
-            raise ValueError("valor deve ser positivo.")
             
+        if Valor < 0 :
+            raise ValueError("valor deve ser positivo.")
+        else:
+            
+            self.Valor= Valor
+
         self.id=id
-        self.Nome = Nome
-        self.Valor = Valor
 
-        p=[]
+        self.Historic.append({
+                    "Id": self.id,
+                    "Servico": self.Nome,
+                    "Valor": self.Valor
+                })
+
         
-        p.append({
-            "Id": self.Valor,
-            "Servico": self.Nome,
-            "Valor do servico": self.Valor
-        })
 
-        def Historico(self,id):
+    def Historico(self,id):
+        if self.Historic is None:
+                return None
+            
+        for item in self.Historic:
+            if item.get("Id") == id:
+                print(item)
+    
+    def Total(self,id):
 
-            for x in p :
+        total_servico=0
 
-                print(x)
-      
+        if self.Historic is None:
+                return None
+            
+        for item in self.Historic:
+            if item.get("Id") == id:
+                total_servico+=item["Valor"]
+
+        print("total em serviço:",total_servico)
+
+
+
+p1 = Oficina()
+
+p1.Registrar(1,"manutencao",230)
+p1.Registrar(1,"manutencao",230)
+p1.Registrar(1,"manutencao",230)
+
+p1.Historico(1)
+p1.Total(1)
+
+#{'Id': 1, 'Servico': 'manutencao', 'Valor': 230}
+#{'Id': 1, 'Servico': 'manutencao', 'Valor': 230}
+#{'Id': 1, 'Servico': 'manutencao', 'Valor': 230}
+#total em serviço: 690
