86f582be0e3f11810bcb842066eef6b771d074ba
Victor Hugo Siqueira Alencar <d2025005350@unifei.edu.br>
2025-12-04 19:38:35 -0300

    Update carro.py

--- carro.py
+++ carro.py
@@ -1,9 +1,19 @@
+from veiculos import Veiculo
 class Carro(Veiculo):
-    def __init__(self, marca: str, modelo: str, ano: int, km_atual: float, num_portas: int):
-        super().__init__(marca, modelo, ano, km_atual)
+    def __init__(self,id, marca: str, modelo: str, ano: int, km_atual: float, num_portas: int):
+        super().__init__(id,marca, modelo, ano, km_atual)
         self.num_portas = num_portas
 
-     def calcular_custo_manutencao(self) -> float:
+    def calcular_custo_manutencao(self) -> float:
         custo_base = 500.00
         penalidade_km = (self.km_atual / 50000) * 100
         return custo_base + penalidade_km
+    
+    def exibir(self):
+        super().exibir()  
+        print(f"Número de portas: {self.num_portas}")
+  
+    def pega(self):
+     dados = super().pega()       # pega tudo da classe Veiculo
+     dados["Número de portas"] = self.num_portas
+     return dados
