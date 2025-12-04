bb4632f9780e44e295c1e1304465b0422a3b27e8
Victor Hugo Siqueira Alencar <d2025005350@unifei.edu.br>
2025-12-04 19:39:24 -0300

    Update moto.py

--- moto.py
+++ moto.py
@@ -1,6 +1,7 @@
+from veiculos import Veiculo
 class Moto(Veiculo):
-    def __init__(self, marca: str, modelo: str, ano: int, km_atual: float, cilindrada: int):
-        super().__init__(marca, modelo, ano, km_atual)
+    def __init__(self, id, marca: str, modelo: str, ano: int, km_atual: float, cilindrada: int):
+        super().__init__(id,marca, modelo, ano, km_atual)
         self.cilindrada = cilindrada
 
    
@@ -8,3 +9,15 @@ class Moto(Veiculo):
         custo_base = 300.00
         bonus_cilindrada = self.cilindrada * 0.5
         return custo_base + bonus_cilindrada
+    
+    def exibir(self):
+        super().exibir()
+        print(f"cilindrada: {self.cilindrada}")
+    
+    def pega(self):
+     dados = super().pega()       # pega tudo da classe Veiculo
+     dados["Cilindradas"] = self.cilindrada
+     return dados
+
+
+    
